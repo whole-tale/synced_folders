eb9118311a38321fe0ed80115496dd3410a51776
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-05-10 20:50:34 -0500

    In theory refresh after import, but it doesn't work...

--- web_client/views/HierarchyWidget.js
+++ web_client/views/HierarchyWidget.js
@@ -2,6 +2,7 @@ import { wrap } from 'girder/utilities/PluginUtils';
 import { getCurrentUser } from 'girder/auth';
 
 import AssetstoreModel from 'girder/models/AssetstoreModel';
+import router from 'girder/router';
 import HierarchyWidget from 'girder/views/widgets/HierarchyWidget';
 
 import SyncedFoldersHierarchyWidget from '../templates/HierarchyWidget.pug';
@@ -21,8 +22,13 @@ wrap(HierarchyWidget, 'render', function (render) {
 });
 
 function _syncFolder(e) {
+    e.preventDefault();
     var assetstore = new AssetstoreModel({ _id: this.parentModel.attributes.assetstoreId });
-    assetstore.import({
+    assetstore.off('g:imported').on('g:imported', function () {
+        router.navigate('folder/' + this.parentModel.id, { trigger: true });
+    }, this).off('g:error').on('g:error', function (err) {
+        this.$('.g-validation-failed-message').text(err.responseJSON.message);
+    }, this).import({
         importPath: this.parentModel.attributes.syncPath,
         destinationId: this.parentModel.id,
         destinationType: 'folder',
